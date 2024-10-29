5c5142fbc3f64b298dc1790bdc4e503c50eed9b4
tharinm <tharindumadhushan864@gmail.com>
2024-10-30 00:46:19 +0530

    connect admin

--- src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
+++ src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -28,4 +31,15 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .authorities("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
 }
