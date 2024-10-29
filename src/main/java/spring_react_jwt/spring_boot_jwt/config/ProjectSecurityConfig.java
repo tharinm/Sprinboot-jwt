783587bc2a8dd629f7ee07200d9a04df4cba1456
tharinm <tharindumadhushan864@gmail.com>
2024-10-29 23:49:16 +0530

    routes blocks

--- src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
+++ src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
@@ -6,17 +6,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // Configuring request authorization rules
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/api/v1/loan/get-my-loan","/api/auth/user").authenticated()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/loan/get-my-loan", "/api/auth/user").authenticated()
                         .requestMatchers("/api/v1/notice/get-my-notice").permitAll()
-                );
+                )
+                // Configuring form-based login
+                .formLogin(withDefaults())
+                // Configuring HTTP Basic authentication
+                .httpBasic(withDefaults());
+
         return http.build();
     }
+
 }
