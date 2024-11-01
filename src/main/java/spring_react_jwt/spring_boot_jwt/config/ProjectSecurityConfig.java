905ca5dc19354b24ec6bbd4cce1a4f59acc7c793
tharinm <tharindumadhushan864@gmail.com>
2024-11-02 00:48:48 +0530

    has role

--- src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
+++ src/main/java/spring_react_jwt/spring_boot_jwt/config/ProjectSecurityConfig.java
@@ -26,8 +26,8 @@ public class ProjectSecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers( "/api/auth/user").hasAuthority("admin")
-                        .requestMatchers("/api/v1/loan/get-my-loan").hasAuthority("user")
+                        .requestMatchers( "/api/auth/user").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/loan/get-my-loan").hasRole("USER")
                         .requestMatchers("/api/v1/notice/get-my-notice","/api/v1/user-login/register").permitAll()
                 )
                 // Configuring form-based login
